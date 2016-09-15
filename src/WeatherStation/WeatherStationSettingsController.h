fc2f0a080fce58cce1d0f512ffed561ae676186b
PiT <pit2000@bk.ru>
2016-09-15 16:38:23 +0300

    Add save weather station settings.

--- src/WeatherStation/WeatherStationSettingsController.h
+++ src/WeatherStation/WeatherStationSettingsController.h
@@ -12,24 +12,49 @@ public:
     WeatherStationSettingsController(QObject *parent = 0);
     ~WeatherStationSettingsController();
 
-    Q_PROPERTY( QString portName READ portName WRITE setPortName NOTIFY portNameChanged)
-
+    //Port
+    Q_PROPERTY( QString portName        READ portName           WRITE setPortName           NOTIFY portNameChanged)
+    //Critcal meteo
+    Q_PROPERTY( qreal temperatureMax    READ temperatureMax     WRITE setTemperatureMax     NOTIFY temperatureMaxChanged )
+    Q_PROPERTY( qreal temperatureMin    READ temperatureMin     WRITE setTemperatureMin     NOTIFY temperatureMinChanged)
+    Q_PROPERTY( qreal precipitationMax  READ precipitationMax   WRITE setPrecipitationMax   NOTIFY precipitationMaxChanged)
+    Q_PROPERTY( qreal windSpeedMax      READ windSpeedMax       WRITE setWindSpeedMax       NOTIFY windSpeedMaxChanged)
 
     Q_PROPERTY( QStringList portNameList READ portNameList NOTIFY portNameListChanged )
 
     QString     portName        (void) { return _portName; }
     QStringList portNameList    (void);
 
+    qreal temperatureMax    (void) { return _temperatureMax;    }
+    qreal temperatureMin    (void) { return _temperatureMin;    }
+    qreal precipitationMax  (void) { return _precipitationMax;  }
+    qreal windSpeedMax      (void) { return _windSpeedMax;      }
+
 private:
     QString     _portName;
     QStringList _portNameList;
 
+    qreal _temperatureMax;
+    qreal _temperatureMin;
+    qreal _precipitationMax;
+    qreal _windSpeedMax;
+
 public slots:
-    void setPortName( QString values ) { _portName = values; }
+    void setPortName        ( QString values ) { _portName = values; }
+
+    void setTemperatureMax  (qreal values);
+    void setTemperatureMin  (qreal values);
+    void setPrecipitationMax(qreal values);
+    void setWindSpeedMax    (qreal values);
 
 signals:
     void portNameChanged     ( QString      str );
     void portNameListChanged ( QStringList  list );
+
+    void temperatureMaxChanged      ( qreal values );
+    void temperatureMinChanged      ( qreal values );
+    void precipitationMaxChanged    ( qreal values );
+    void windSpeedMaxChanged        ( qreal values );
 };
 
 #endif // WEATHERSTATIONSETTINGSCONTROLLER_H
